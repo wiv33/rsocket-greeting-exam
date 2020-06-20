70f36c79aee451e3ffabad82467c3c4208abca2e
ps <wiv821253@gmail.com>
2020-06-20 18:51:09 +0900

    optimizer

--- producer/src/main/java/org/psawesome/producer/rsocket/MessageController.java
+++ producer/src/main/java/org/psawesome/producer/rsocket/MessageController.java
@@ -2,7 +2,6 @@ package org.psawesome.producer.rsocket;
 
 import org.psawesome.producer.dto.GreetRequest;
 import org.psawesome.producer.dto.GreetResponse;
-import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
